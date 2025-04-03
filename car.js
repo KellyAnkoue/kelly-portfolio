6a11955e9b38ff3e8ae4689006ca69d5f8282fdc
KellyAnkoue <143815174+KellyAnkoue@users.noreply.github.com>
2025-04-02 20:38:25 -0400

    Add files via upload

---
+++ car.js
@@ -0,0 +1,8 @@
+const carouselImages = [
+    "images/Dark Green Modern Motivational Quote Instagram Post.jpg",
+    "images/Spring 2025 Tryouts.jpg",
+    "images/3 (1).png",
+    "images/Frame b.png",
+    "images/Frame h (1).png"
+  ];
+  
